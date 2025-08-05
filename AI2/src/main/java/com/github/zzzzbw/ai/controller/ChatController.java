50502801d2c49016544ec976fad29889ce280e8e
janet675 <2372258346@qq.com>
2025-08-05 09:03:54 +0800

    Add files via upload

---
+++ AI2/src/main/java/com/github/zzzzbw/ai/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.github.zzzzbw.ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+    private final ChatClient chatClient;//自动注入
+    @RequestMapping(value = "chat",produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String prompt, String chatId){
+        // 链式调用：构建提示 -> 发送请求 -> 获取文本响应
+        return chatClient.prompt()
+                .user(prompt)
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
+    }
+}
