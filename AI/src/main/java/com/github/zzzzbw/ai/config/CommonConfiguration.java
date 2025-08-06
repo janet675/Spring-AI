e4812a271c67767fc1558d7e57830b66b4ff7f8e
lyk <2372258346@qq.com>
2025-08-06 11:19:28 +0800

    rag

--- AI/src/main/java/com/github/zzzzbw/ai/config/CommonConfiguration.java
+++ AI/src/main/java/com/github/zzzzbw/ai/config/CommonConfiguration.java
@@ -1,19 +1,57 @@
 package com.github.zzzzbw.ai.config;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
+import io.milvus.client.MilvusServiceClient;
+import io.milvus.param.ConnectParam;
+import io.milvus.param.IndexType;
+import io.milvus.param.MetricType;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.TokenCountBatchingStrategy;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.milvus.MilvusVectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
-public class CommonConfiguration  {
+public class CommonConfiguration {
+
+    @Value("${milvus.host}")
+    private String host;
+    @Value("${milvus.port}")
+    private Integer port;
+
+    /**
+     * 定义一个名为 milvusServiceClient 的Bean，用于创建并返回一个 MilvusServiceClient 实例。
+     */
     @Bean
-    public ChatClient chatClient(OllamaChatModel model)//依赖的模型
-    {
-        return ChatClient
-                .builder(model)
-                .defaultSystem("你是一个热心的人,你的名字叫小团团,请以小团团的身份和语气回答我的问题")//提示词
+    public MilvusServiceClient milvusServiceClient() {
+        return new MilvusServiceClient(
+                ConnectParam.newBuilder()
+                        .withHost(host)
+                        .withPort(port)
+                        .build());
+    }
+
+    /**
+     * 定义一个名为 vectorStore2 的Bean，用于创建并返回一个 VectorStore 实例。
+     * 使用 MilvusVectorStore.builder 方法构建向量存储对象，并设置以下参数：
+     * collectionName：集合名称为 "vector_store_02"。
+     * databaseName：数据库名称为 "default"。
+     * embeddingDimension：嵌入维度为 1536。
+     * indexType：索引类型为 IVF_FLAT，这是一种常见的近似最近邻搜索索引类型。
+     * metricType：度量类型为 COSINE，用于计算向量之间的余弦相似度。
+     * batchingStrategy：使用 TokenCountBatchingStrategy 策略进行批量处理。
+     * initializeSchema：设置为 true，表示在构建时初始化数据库模式。
+     */
+    @Bean(name = "vectorStore2")
+    public VectorStore vectorStore(MilvusServiceClient milvusClient, EmbeddingModel embeddingModel) {
+        return MilvusVectorStore.builder(milvusClient, embeddingModel)
+                .collectionName("vector_store_02")
+                .databaseName("default")
+                .embeddingDimension(1536)
+                .indexType(IndexType.IVF_FLAT)
+                .metricType(MetricType.COSINE)
+                .batchingStrategy(new TokenCountBatchingStrategy())
+                .initializeSchema(true)
                 .build();
     }
 }
